621cc68f3e6e93eaff179f8ccbbc1a509d4846c0
Cyber-Savant <chireigns46@gmail.com>
2025-12-10 00:09:34 +0100

    Secure deployment config for NestJS

--- src/main.ts
+++ src/main.ts
@@ -26,7 +26,7 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(port);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
   console.log(`✅✅ Application is running on: http://localhost:${port}`);
   console.log(`✅✅ Google OAuth: http://localhost:${port}/auth/google`);
 }
